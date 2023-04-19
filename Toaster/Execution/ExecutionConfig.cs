8f180d2ee64d08dda38bbc68d7b11d2f5fb7d252
Fae Burns <fae.burns0@gmail.com>
2023-04-19 22:22:22 +0100

    Made ExecutionConfig compatible with unity editor

--- Toaster/Execution/ExecutionConfig.cs
+++ Toaster/Execution/ExecutionConfig.cs
@@ -6,30 +6,31 @@ namespace Toaster.Execution;
 /// <summary>
 /// A class containing information about the setup of an <see cref="Interpreter"/> and what functions it can support.
 /// </summary>
+[Serializable]
 public class ExecutionConfig
 {
     /// <summary>
     /// Gets a collection of the names of all additional registers.
     /// </summary>
-    public List<string> NamedRegisters { get; } = new List<string>();
+    public List<string> NamedRegisters = new List<string>();
 
     /// <summary>
     /// Gets or Sets the amount of ($s0) stack registers on the interpreter.
     /// </summary>
-    public int StackRegisterCount { get; set; }
+    public int StackRegisterCount = 0;
 
     /// <summary>
     /// Gets or Sets the maximum stack depth of the interpreter.
     /// </summary>
-    public int MaxStackDepth { get; set; }
+    public int MaxStackDepth = 0;
 
     /// <summary>
     /// Gets or Sets the amount of pins the interpreter has access to.
     /// </summary>
-    public int PinCount { get; set; }
+    public int PinCount = 0;
 
     /// <summary>
     /// Gets or Sets the amount of ($r0) basic registers the interpreter has.
     /// </summary>
-    public int BasicRegisterCount { get; set; }
+    public int BasicRegisterCount = 0;
 }
